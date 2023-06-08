4806aade5e0569fb421f3ea8cbc747b518004707
vamon ngwa <vamon.ngwa@janus-ai.com>
2023-06-08 12:22:48 -0400

    added a new stack

--- bin/cdk.ts
+++ bin/cdk.ts
@@ -4,8 +4,8 @@ import * as cdk from 'aws-cdk-lib';
 import { RdsSnapshotExportPipelineStack, RdsEventId, RdsSnapshotType } from '../lib/rds-snapshot-export-pipeline-stack';
 
 const app = new cdk.App();
-new RdsSnapshotExportPipelineStack(app, 'VN-RdsSnapshotExportToS3Pipesline', {
-  dbName: 'dev-observe-database',
+new RdsSnapshotExportPipelineStack(app, 'RdsSnapshotExportToS3Pipesline-Test-vamon', {
+  dbName: 'dev-visionxdb-cluster',
   rdsEvents:
     [
       {
@@ -19,10 +19,14 @@ new RdsSnapshotExportPipelineStack(app, 'VN-RdsSnapshotExportToS3Pipesline', {
       {
         rdsEventId: RdsEventId.DB_BACKUP_SNAPSHOT_FINISHED_COPY,
         rdsSnapshotType: RdsSnapshotType.DB_BACKUP_SNAPSHOT
+      },
+      {
+        rdsEventId: RdsEventId.DB_AUTOMATED_AURORA_SNAPSHOT_CREATED,
+        rdsSnapshotType: RdsSnapshotType.DB_AUTOMATED_SNAPSHOT
       }
 
     ],
-  s3BucketName: 'janus-s3-rdssnapshots-testbucketstest-VN',
+  s3BucketName: 'janus-s3-rdssnapshots-testbucketstest-vamon',
 });
 
 
